d3998b663c8eb7c9d24eb07fada40aa6d05aa49b
tdup-torn <radigou29@gmail.com>
2026-02-25 22:37:30 -0500

    V9.4.1 - Remove sorting buttons from Raid & Walls

--- battle_stats_predictor.user.js
+++ battle_stats_predictor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Battle Stats Predictor
 // @description Show battle stats prediction, computed by a third party service
-// @version     9.4.0
+// @version     9.4.1
 // @namespace   tdup.battleStatsPredictor
 // @updateURL   https://github.com/tdup-torn/userscripts/raw/master/battle_stats_predictor.user.js
 // @downloadURL https://github.com/tdup-torn/userscripts/raw/master/battle_stats_predictor.user.js
@@ -3284,6 +3284,14 @@ function InjectSortButtons(node) {
             return;
     }
 
+    el = Array.from(el).filter(e => {
+        if (e.closest('.raid-members-list')) return false; // Exclude raid.
+
+        const descWrap = e.closest('.desc-wrap');
+        if (descWrap && !descWrap.matches('[class*="warDesc"]')) return false; // Exclude walls.
+
+        return true;
+    });
 
     let buttonsArray = [];
     for (let i = 0; i < el.length; ++i) {
